0dbd0add9c9bcb8d1ecf61d83f0174a71dd9f6ba
Muhamad Rifqi Adrian <rifqiadrian0416@gmail.com>
2026-01-19 17:09:48 +0700

    fix smtp

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,11 +5,17 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.google.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 exports.register = async (req, res) => {
